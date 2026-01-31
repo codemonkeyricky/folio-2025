76d6f35357cacf67fd8c18809ad1e95ccfcb6b01
Richard <codemonkeyricky@gmail.com>
2026-01-30 18:16:54 -0800

    migrate: ralph-loop

--- loop.sh
+++ loop.sh
@@ -1,3 +1,4 @@
 for i in {1..10}; do
-  ccr code  --dangerously-skip-permissions -p "Follow instructions in js-to-ts.txt" --output-format stream-json --verbose --include-partial-messages
+  filename=$(find sources/ | grep js$ | xargs wc | sort -hsr | tail -n1 | awk '{print $NF}')
+  ccr code  --dangerously-skip-permissions -p "Run js-to-ts.txt against $filename" --output-format stream-json --verbose --include-partial-messages
 done
