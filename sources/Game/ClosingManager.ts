6433c338a37be24c96e0d36a84ae5fc49a4013ee
Richard <codemonkeyricky@gmail.com>
2026-01-30 21:19:58 -0800

    fix: add null checks for ClosingManager.ts

--- sources/Game/ClosingManager.ts
+++ sources/Game/ClosingManager.ts
@@ -11,33 +11,36 @@ export class ClosingManager
         this.game = Game.getInstance()
 
         // Pause input => Close menu or open menu intro
-        this.game.inputs.events.on('pause', (action: any) =>
-        {
-            if(action.active)
+        if(this.game.inputs?.events)
+            this.game.inputs.events.on('pause', (action: any) =>
             {
-                if(this.game.menu.state === Menu.OPEN || this.game.menu.state === Menu.OPENING)
+                if(action.active)
                 {
-                    this.game.menu.close()
+                    if(this.game.menu?.state === Menu.OPEN || this.game.menu?.state === Menu.OPENING)
+                    {
+                        this.game.menu?.close()
+                    }
+                    else
+                    {
+                        this.game.menu?.open()
+                    }
                 }
-                else
-                {
-                    this.game.menu.open()
-                }
-            }
-        })
+            })
 
         // On modal open => Close menu
-        this.game.modals.events.on('open', () =>
-        {
-            if(this.game.menu.state === Menu.OPEN || this.game.menu.state === Menu.OPENING)
-                this.game.menu.close()
-        })
+        if(this.game.modals?.events)
+            this.game.modals.events.on('open', () =>
+            {
+                if(this.game.menu?.state === Menu.OPEN || this.game.menu?.state === Menu.OPENING)
+                    this.game.menu?.close()
+            })
 
         // On menu open => Close modal
-        this.game.menu.events.on('open', () =>
-        {
-            if(this.game.modals.state === Modals.OPEN || this.game.modals.state === Modals.OPENING)
-                this.game.modals.close()
-        })
+        if(this.game.menu?.events)
+            this.game.menu.events.on('open', () =>
+            {
+                if(this.game.modals?.state === Modals.OPEN || this.game.modals?.state === Modals.OPENING)
+                    this.game.modals?.close()
+            })
     }
 }
